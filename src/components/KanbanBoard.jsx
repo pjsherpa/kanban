d54d004fdd7ba45a59f9ceb2c1fc1fff8d1dfd4c
Phinjock Sherpa <phinjock@gmail.com>
2023-10-03 16:39:17 -0700

    created a dragable section in the KanbanBoard

--- src/components/KanbanBoard.jsx
+++ src/components/KanbanBoard.jsx
@@ -1,1 +1,17 @@
 import React,{useState} from "react";
+import { DragDropContext } from "react-beautiful-dnd";
+
+const KanbanBoard = () => {
+  const [completed, setCompleted]=useState([])
+  const [incomplete,setIncomplete]=useState([])
+  return (
+   <DragDropContext>
+<h2 style={{textAlign:'center'}}>PROGRESS BOARD</h2>
+<div style={{display:'flex', justifyContent:'space-between', alignItems:'center', flexDirection:'row'}}>
+
+</div>
+   </DragDropContext>
+  )
+}
+
+export default KanbanBoard
